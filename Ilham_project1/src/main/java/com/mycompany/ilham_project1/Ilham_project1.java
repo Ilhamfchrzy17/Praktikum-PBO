db46311a732166c34a5a8ed9312d7b3f0b5ad036
Maulana Ilham Fachrezy <maulana.ilham.fachrezy@students.untidar.ac.id>
2024-11-29 00:39:07 +0700

    Add files via upload

---
+++ Ilham_project1/src/main/java/com/mycompany/ilham_project1/Ilham_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ilham_project1;
+
+/**
+ *
+ * @author fchrzy
+ */
+public class Ilham_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("• Nama  : Maulana Ilham Fachrezy");
+        System.out.println("• NPM   : 2320506022");
+        System.out.println("• Alamat: Magersari");
+        System.out.println("• No_HP : 081229038195");
+    }
+}
